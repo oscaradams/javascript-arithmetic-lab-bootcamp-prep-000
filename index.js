13756579a607324bb67a33bbe4afffb87b143431
Oscar Adams <oadams.co@gmail.com>
2018-06-29 13:21:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,26 @@ function add(a, b){
   
 }
 
+function subtract(a, b){
+  
+  return a - b
+  
+  
+  
+}
+
+function multiply(a, b){
+  
+  return a*b
+  
+  
+  
+}
+
+function divide(a, b){
+  
+  return a/b
+  
+  
+  
+}
